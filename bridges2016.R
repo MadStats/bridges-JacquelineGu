e0f10e341902d7cba6b98e2a9e5761c4ba00a507
JacquelineGu <helen_gsr@outlook.com>
2017-01-25 17:24:14 -0600

    Delete bridges2016.R

--- bridges2016.R
+++
@@ -1,6 +0,0 @@
-##setwd("C:\\Users\\gsr\\Desktop\\everything\\威斯康辛\\479-1 Data Science in R\\2016hwybronlyonefile")
-data2016<-read.csv("slubkin_992016-20170106140351.txt",header = TRUE,sep=",")
-data2016.new=data.frame(StateCode=data2016$STATE_CODE_001,HighwayAgencyDistrict=data2016$HIGHWAY_DISTRICT_002,
-                        PlaceCode=data2016$PLACE_CODE_004,RecordType=data2016$RECORD_TYPE_005A,
-                        FacilityCarried=data2016$FACILITY_CARRIED_007,StructureNumber=data2016$STRUCTURE_NUMBER_008,
-                        Location=data2016$LOCATION_009,KilometerPoint=data2016$KILOPOINT_011,Latitude=data2016$LAT_016)
